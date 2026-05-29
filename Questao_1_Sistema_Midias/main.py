e9657e60c02080476c973625f951b2ee2992abb1
Ricky Brendon <rickybrendon3@gmail.com>
2026-05-29 13:40:27 -0300

    Update main.py

--- Questao_1_Sistema_Midias/main.py
+++ Questao_1_Sistema_Midias/main.py
@@ -1,1 +1,14 @@
+from video import Video
+from podcast import Podcast
+from video_narrado import VideoNarrado
 
+midias = [
+    Video("Aula de Python", 30),
+    Podcast("Podcast Tech", 45),
+    VideoNarrado("Documentário", 60)
+]
+
+for midia in midias:
+    midia.mostrar_info()
+    midia.reproduzir()
+    print("------")
